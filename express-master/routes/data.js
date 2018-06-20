32a6c4ba234840b63f4650fc265954b52549ddac
zxhorz <zhouxinhao666@gmai.com>
2018-06-20 09:11:02 +0800

    2018/6/20 zxh

--- express-master/routes/data.js
+++ express-master/routes/data.js
@@ -14,153 +14,36 @@ connection.connect();
 
 var sql = 'SELECT * FROM user';
 
-//读取数据模块，供客户端调用
-//查询接口，token校验
-//公共接口，无需校验
-//data/read?type=it
-//data/read?type=it.json
-router.get('/read', function(req, res, next) {
-    var type = req.param('type') || "";
-    fs.readFile(PATH + type + '.json', function(err, data) {
-        if (err) {
-            return res.send({
-                status: 0,
-                info: '读取文件异常'
-            });
-        }
-        var COUNT = 50;
-        // TODO: try{}catch(){}
-        var obj = [];
-        try {
-            obj = JSON.parse(data.toString());
-        } catch (e) {
-            obj = [];
-        }
-        if (obj.length > COUNT) {
-            obj = obj.slice(0, COUNT);
-        }
-        return res.send({
-            status: 1,
-            data: obj
-        });
-    });
-});
-
-
-// 数据存储模块——后台开发使用
-router.post('/write', function(req, res, next) {
-    if (!req.cookies.user) {
-        return res.render('login', {});
-    }
-    // 文件名
-    var type = req.param('type') || "";
-    // 关键字段
-    var url = req.param('url') || '';
-    var title = req.param('title') || '';
-    var img = req.param('img') || '';
-    if (!type || !url || !title || !img) {
-        return res.send({
-            status: 0,
-            info: '提交的字段不全'
-        });
-    }
-    //1)读取文件
-    var filePath = PATH + type + '.json';
-    fs.readFile(filePath, function(err, data) {
-        if (err) {
-            return res.send({
-                status: 0,
-                info: '读取数据失败'
-            });
-        }
-        var arr = JSON.parse(data.toString());
-        //代表每一条记录
-        var obj = {
-            img: img,
-            url: url,
-            title: title,
-            id: guidGenerate(),
-            time: new Date()
-        };
-        arr.splice(0, 0, obj);
-        //2)写入文件
-        var newData = JSON.stringify(arr);
-        fs.writeFile(filePath, newData, function(err) {
-            if (err) {
-                return res.send({
-                    status: 0,
-                    info: '写入文件失败'
-                });
-            }
-            return res.send({
-                status: 1,
-                info: obj
-            });
-        });
-    });
-});
-
-//阅读模块写入接口 后台开发使用
-router.post('/write_config', function(req, res, next) {
-    if (!req.cookies.user) {
-        return res.render('login', {});
-    }
-    //TODO:后期进行提交数据的验证
-    //防xss攻击 xss
-    // npm install xss
-    // require('xss')
-    // var str = xss(name);
-    var data = req.body.data;
-    //TODO ： try catch
-    var obj = JSON.parse(data);
-    var newData = JSON.stringify(obj);
-
-    // 写入
-    fs.writeFile(PATH + 'config.json', newData, function(err, data) {
-        if (err) {
-            return res.send({
-                status: 0,
-                info: '写入数据失败'
-            });
-        }
-        return res.send({
-            status: 1,
-            info: '数据写入成功',
-            data: newData
-        })
-    })
-});
-
 
 //提交表单接口
 router.post('/submit', function(req, res, next) {
     //用户名、email、subject、info
     if (!req.cookies.user) {
-         return res.send({
+        return res.send({
             status: 0,
             info: '未登录'
-        }); 
+        });
     }
     var username = req.body.username;
     var email = req.body.email;
     var subject = req.body.subject;
     var info = req.body.info;
 
-    sql = 'INSERT INTO register(username,email,subject,info) VALUES(?,?,?,?)';
-    var  addSqlParams = [username, email,subject,info];
-    connection.query(sql,addSqlParams, function(err, result) {
-        if(err){
-        console.log('[INSERT ERROR] - ',err.message);
+    sql = 'INSERT INTO submit(username,name,email,subject,info,check) VALUES(?,?,?,?,?,0)';
+    var addSqlParams = [username, req.cookies.user, email, subject, info];
+    connection.query(sql, addSqlParams, function(err, result) {
+        if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
             return res.send({
                 status: 0,
                 info: '写入数据失败'
             });
-        }        
- 
+        }
+
         console.log('--------------------------INSERT----------------------------');
         //console.log('INSERT ID:',result.insertId);        
-        console.log('INSERT ID:',result);        
-        console.log('-----------------------------------------------------------------\n\n'); 
+        console.log('INSERT ID:', result);
+        console.log('-----------------------------------------------------------------\n\n');
         return res.send({
             status: 1,
             info: '数据写入成功'
@@ -170,8 +53,112 @@ router.post('/submit', function(req, res, next) {
 });
 
 
+//用户查询接口
+router.post('/query', function(req, res, next) {
+    // req.cookies.user
+    var username = req.cookies.user;
 
-//提交表单接口
+    sql = "select * from submit where username = ?";
+    var SqlParams = [username];
+    connection.query(sql, SqlParams,function(err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return res.send({
+                status: 0,
+                info: '查询失败'
+            });
+        }
+
+        console.log('--------------------------SELECT----------------------------');
+        var obj = new Array();
+        for (var i = 0; i < result.length; i++) {
+            obj.push({
+                name: result[i].name,
+                email: result[i].email,
+                subject: result[i].subject,
+                info: result[i].info,
+                check: result[i].check    
+            });
+        }    
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+        return res.send({
+            status: 1,
+            info: obj
+        });
+    });
+
+});
+
+
+//用户查询接口
+router.post('/admin_query', function(req, res, next) {
+    // req.cookies.user
+    var username = req.cookies.user;
+
+    sql = "select * from submit";
+    connection.query(sql,function(err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return res.send({
+                status: 0,
+                info: '查询失败'
+            });
+        }
+
+        console.log('--------------------------SELECT----------------------------');
+        var obj = new Array();
+        for (var i = 0; i < result.length; i++) {
+            obj.push({
+                name: result[i].name,
+                email: result[i].email,
+                subject: result[i].subject,
+                info: result[i].info,
+                check: result[i].check    
+            });
+        }    
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+        return res.send({
+            status: 1,
+            info: obj
+        });
+    });
+
+});
+
+//用户查询接口
+router.post('/info_query', function(req, res, next) {
+    // req.cookies.user
+    var name = req.body.name;
+    var subject = req.body.subject;
+
+    sql = "select * from submit where name = ? and subject = ?";
+    var SqlParams = [name, subject];
+    connection.query(sql,SqlParams,function(err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return res.send({
+                status: 0,
+                info: '查询失败'
+            });
+        }
+
+        console.log('--------------------------SELECT----------------------------');
+        var info = result[0].info;
+
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+        return res.send({
+            status: 1,
+            info: info
+        });
+    });
+
+});
+
+
+//注册接口
 router.post('/register', function(req, res, next) {
     //用户名、email、subject、info
     var username = req.body.username;
@@ -179,23 +166,32 @@ router.post('/register', function(req, res, next) {
     var password = req.body.password;
 
     sql = 'INSERT INTO user(username,email,password) VALUES(?,?,?)';
-    var  addSqlParams = [username,email,password];
-    connection.query(sql,addSqlParams, function(err, result) {
-        if(err){
-        console.log('[INSERT ERROR] - ',err.message);
-        var info="注册失败"
-        if(/Duplicate entry/.test(err.message))
-            info = "用户名已被注册";
+    var addSqlParams = [username, email, password];
+    connection.query(sql, addSqlParams, function(err, result) {
+        if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
+            var info = "注册失败"
+            if (/Duplicate entry/.test(err.message))
+                info = "用户名已被注册";
             return res.send({
                 status: 0,
                 info: info
             });
-        }        
- 
+        }
+
         console.log('--------------------------INSERT----------------------------');
         //console.log('INSERT ID:',result.insertId);        
-        console.log('INSERT ID:',result);        
-        console.log('-----------------------------------------------------------------\n\n'); 
+        for (var i = 0; i < result.length; i++) {
+            
+            if (username === result[i].username && password === result[i].password) {
+                res.cookie('user', username);
+                return res.send({
+                    status: 1
+                });
+            }
+        }
+        console.log('INSERT ID:', result);
+        console.log('-----------------------------------------------------------------\n\n');
         return res.send({
             status: 1,
             info: '注册成功'
@@ -210,11 +206,6 @@ router.post('/login', function(req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
 
-    //TODO ：对用户名、密码进行校验
-    //xss处理、判空
-
-    //密码加密 md5(md5(password + '随机字符串'))
-    //密码需要加密－> 可以写入JSON文件
 
     sql = "select username,password from user";
     connection.query(sql, function(err, result) {
@@ -227,7 +218,7 @@ router.post('/login', function(req, res, next) {
         }
 
         console.log('--------------------------SELECT----------------------------');
-        for (var i = 0; i < result.length; i++){
+        for (var i = 0; i < result.length; i++) {
             console.log(result[0]);
             if (username === result[i].username && password === result[i].password) {
                 res.cookie('user', username);
@@ -244,13 +235,7 @@ router.post('/login', function(req, res, next) {
     });
 });
 
-//guid
-function guidGenerate() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    }).toUpperCase();
-}
+
 
 module.exports = router;
+
--- express-master/routes/index.js
+++ express-master/routes/index.js
@@ -10,65 +10,87 @@ router.get('/', function(req, res, next) {
     title: '首页'
   });
 });
+router.get('/index', function(req, res, next) {
+  res.render('index', {
+    title: '首页'
+  });
+});
 
 router.get('/login', function(req, res, next) {
+  res.clearCookie('user');
   res.render('login', {
     title: '登录'
   });
 });
 
-router.get('/tuijian', function(req, res, next) {
+router.get('/user', function(req, res, next) {
   if (!req.cookies.user) {
     return res.render('login', {});
   }
-  res.render('tuijian', {});
-});
-
-router.get('/edit', function(req, res, next) {
-  // if (!req.cookies.user) {
-  //   return res.render('login', {});
-  // }
-  var type = req.query.type;
-  if (type) {
-    var obj = {};
-    switch (type) {
-      case 'sanwen':
-        obj = {};
-        break;
-      case 'it':
-        obj = {};
-        break;
-      case 'manager':
-        obj = {};
-        break;
-      case 'cookies':
-        obj = {};
-        break;
-      default:
-        return res.send({
-          status: 0,
-          info: '参数错误'
-        });
-        break;
-    }
-    fs.readFile(PATH + type + '.json', (err, data) => {
-      if (err) {
-        return res.send({
-          status: 0,
-          info: 'fail.....'
-        });
-      }
-      var obj = JSON.parse(data.toString());
-      return res.render('edit', {
-        data: obj
-      });
-    });
-  } else {
-    return res.send({
-      status: 0,
-      info: '参数错误'
+  else if(req.cookies.user == "admin"){
+    res.render('admin', {
+      title: '管理中心'
     });
   }
+  else{
+    res.render('user', {
+      title: '个人中心'
+    });
+  }
+});
+
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', {
+    title: '选项页面'
+  });
+});
+
+router.get('/emei', function(req, res, next) {
+  res.render('emei', {
+    title: '峨眉'
+  });
+});
+
+router.get('/huashan', function(req, res, next) {
+  res.render('huashan', {
+    title: '华山'
+  });
+});
+
+router.get('/kunlun', function(req, res, next) {
+  res.render('kunlun', {
+    title: '昆仑'
+  });
+});
+
+router.get('/wudang', function(req, res, next) {
+  res.render('wudang', {
+    title: '武当'
+  });
+});
+
+router.get('/qingcheng', function(req, res, next) {
+  res.render('qingcheng', {
+    title: '青城'
+  });
+});
+
+router.get('/kongtong', function(req, res, next) {
+  res.render('kongtong', {
+    title: '崆峒'
+  });
+});
+
+router.get('/quanzhen', function(req, res, next) {
+  res.render('quanzhen', {
+    title: '全真'
+  });
+});
+
+router.get('/shaolin', function(req, res, next) {
+  res.render('shaolin', {
+    title: '少林'
+  });
 });
 
 module.exports = router;
